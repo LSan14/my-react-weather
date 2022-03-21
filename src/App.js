4c728a8c565b1465c6123f3dcd779927fce3d099
Lizeth Sandoval Valladares <liz.sandoval09@gmail.com>
2022-03-21 13:08:58 -0700

    Adding text inside the js.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Weather from "./Weather";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
@@ -6,7 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1> Weather Search </h1>
+        <Weather />
         <footer>
           This project is coded by Lizeth Sandoval and is open-source in{" "}
           <a href="https://github.com/LSan14/my-react-weather" target="_blank">
---
+++ src/Weather.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div clasName="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-dark w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <h1> Los Angeles, CA </h1>
+      <ul>
+        <li>Monday 12:00 PM</li>
+        <li>Sunny</li>
+      </ul>
+      <div className="row">
+        <div clasName="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+            alt="Sunny"
+          />
+          71 °F
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 0% </li>
+            <li> Wind: 4 mph </li>
+            <li> Humidity: 35% </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
