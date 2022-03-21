a20139e0311d17fa2f2e4f495aa93dcb83777def
Lizeth Sandoval Valladares <liz.sandoval09@gmail.com>
2022-03-21 11:55:46 -0700

    Added a footer and link to github.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Test </h1>
+      <h1>Weather Search </h1>
+      <footer>
+        This project is coded by Lizeth Sandoval and is open-source in{" "}
+        <a href="https://github.com/LSan14/my-react-weather" target="_blank">
+          {""} Github.
+        </a>
+      </footer>
     </div>
   );
 }
