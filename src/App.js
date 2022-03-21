6f14247ff415de1de5cbd3fe6dc575abd7b695d8
Lizeth Sandoval Valladares <liz.sandoval09@gmail.com>
2022-03-21 12:15:43 -0700

    Added bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather Search </h1>
-      <footer>
-        This project is coded by Lizeth Sandoval and is open-source in{" "}
-        <a href="https://github.com/LSan14/my-react-weather" target="_blank">
-          {""} Github.
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather Search </h1>
+        <footer>
+          This project is coded by Lizeth Sandoval and is open-source in{" "}
+          <a href="https://github.com/LSan14/my-react-weather" target="_blank">
+            {""} Github.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
