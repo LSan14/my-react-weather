dc09b13d41d3ac905732e511da23c43f32eb1380
Lizeth Sandoval Valladares <liz.sandoval09@gmail.com>
2022-03-21 17:55:46 -0700

    Fixed bugs . (col-6)

--- src/Weather.js
+++ src/Weather.js
@@ -6,20 +6,16 @@ export default function Weather() {
     <div className="Weather">
       <form>
         <div className="row">
-          <div clasName="col-9">
+          <div className="col-9">
             <input
               type="search"
-              placeholder="Enter a city.."
+              placeholder="Enter city..."
               className="form-control"
               autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
+            <input type="submit" value="Search" className="btn btn-primary" />
           </div>
         </div>
       </form>
@@ -29,7 +25,7 @@ export default function Weather() {
         <li>Sunny</li>
       </ul>
       <div className="row">
-        <div clasName="col-6">
+        <div className="col-6">
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
             alt="Sunny"
