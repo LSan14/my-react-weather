e1077ebddfbb5f77053b1f3b34e67ebb70bb06f5
Lizeth Sandoval Valladares <liz.sandoval09@gmail.com>
2022-03-25 16:48:57 -0700

    Fixed the icoURL to icon.

--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,7 @@ export default function Weather(props) {
       windy: response.data.wind.speed,
       city: response.data.name,
       description: response.data.weather[0].description,
-      iconURL: response.data.weather[0].icon,
+      icon: response.data.weather[0].icon,
     });
   }
 
